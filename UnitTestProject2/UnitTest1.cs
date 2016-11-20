518e13a72689aaed00104315e4131e8777d80f63
Rostislav <rostandslav@yandex.ru>
2016-11-21 00:00:45 +0500

    Добавление файлов проекта

---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace UnitTestProject2
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void OpenAppTest()
+        {
+            driver.Url = "http://www.demoqa.com";
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+    }
+}
